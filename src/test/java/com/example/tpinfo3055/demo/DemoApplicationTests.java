5a2f524125954bf271b9eb616c9a17dd76bcf98a
Tomdieu <ivantomdio@gmail.com>
2023-01-10 22:08:16 +0100

    initial commit

---
+++ src/test/java/com/example/tpinfo3055/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpinfo3055.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
