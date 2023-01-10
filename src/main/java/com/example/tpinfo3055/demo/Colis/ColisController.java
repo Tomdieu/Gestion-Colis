fc28477eb999ed916d987ebdb92aa0de45e47f76
Tomdieu <ivantomdio@gmail.com>
2023-01-10 22:57:43 +0100

    Remove Autowired

--- src/main/java/com/example/tpinfo3055/demo/Colis/ColisController.java
+++ src/main/java/com/example/tpinfo3055/demo/Colis/ColisController.java
@@ -1,6 +1,5 @@
 package com.example.tpinfo3055.demo.Colis;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +10,6 @@ public class ColisController {
 
     private final ColisService colisService;
 
-    @Autowired // This is a dependency injection
     public ColisController(ColisService colisService) {
         this.colisService = colisService;
     }
